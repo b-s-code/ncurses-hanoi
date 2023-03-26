fadf2af0f6b0416f559d701d141204e2b0b6e25f
b-s-code <89502375+b-s-code@users.noreply.github.com>
2023-03-26 16:11:57 +0800

    fix bug, finish frontend, make shippable

--- src/my_hanoi.c
+++ src/my_hanoi.c
@@ -56,15 +56,10 @@ typedef enum disk pile[3];
 int lowest_empty_slot(pile p);
 
 /*
-* "properly" means biggest to smallest, from the bottom up
-*/
-bool is_stacked_properly(pile p);
-
-/*
 * returns pointer to left, middle, or right pile
 * caller specifies whether they want candidate pile (1), or current pile (0)
 */
-enum disk* get_pile_by_label(char c, bool should_return_candidate_pile);
+enum disk* get_pile_by_label(char c);
 
 /*
 * there are two of these game datasets, to help manage transitions
@@ -74,13 +69,25 @@ struct game_state_data
     pile lhs;
     pile middle;
     pile rhs;
-} game_data_current, game_data_candidate_new;
+} game_data_current;
 
 /*
 * for example, {'l', 'r'} means move the topmost disk from
 * the left pile to the topmost unused position on the right pile
 */
 unsigned char command[2];
+
+/*
+* front-end functions
+*/
+void print_spacer();
+void print_null_disk();
+void print_red_disk();
+void print_orange_disk();
+void print_yellow_disk();
+void print_base_plate_disk();
+void print_disk_scene();
+void print_player_prompt();
 
 int main()
 {
@@ -114,6 +121,18 @@ void start_game()
     // put the terminal into required mode (needs to be undone at end of game)
     initscr();
 
+    // avoid having colour pairs defined all over the place
+    start_color();
+	init_pair(1, COLOR_WHITE, COLOR_BLACK); // should be called at the end of any function that calls attron() with an argument of COLOR_PAIR
+	init_pair(2, COLOR_RED, COLOR_RED);
+    init_color(COLOR_BLUE, 1000, 500, 0); // there's no COLOR_ORANGE, so we repurpose COLOR_BLUE 
+	init_pair(4, COLOR_BLUE, COLOR_BLUE);
+	init_pair(3, COLOR_YELLOW, COLOR_YELLOW);
+    init_color(COLOR_MAGENTA, 300, 300, 300); // there's no COLOR_GREY, so we repurpose COLOR_MAGENTA 
+	init_pair(5, COLOR_MAGENTA, COLOR_MAGENTA);
+	init_pair(6, COLOR_BLACK, COLOR_BLACK);
+    attron(COLOR_PAIR(1));
+    
     // get characters directly from user, no line buffering
     raw();
 
@@ -130,9 +149,6 @@ void start_game()
     game_data_current.rhs[0] = null_disk;
     game_data_current.rhs[1] = null_disk;
     game_data_current.rhs[2] = null_disk;
-
-    // will update this in assess()
-    game_data_candidate_new = game_data_current;
 }
 
 void stop_game()
@@ -149,34 +165,29 @@ void greet()
     printw(".==========================.\n");
     printw("\nWelcome!\n\nPress any key to start.");
     
-    // essentially a buffer swap
     refresh();
 
     // wait until a user has pressed a key to start
     getch();
-    
-    // TODO : move this to somewhere in the process() function
-    game_state = won;
+    game_state = waiting;
 }
 
 void wait()
 {
-    // get rid of anything currently being displayed
     clear();
 
-    // TODO : actually display towers here
-    // ... maybe describe how to input commands too?
-    // ... actually also need to describe the win condition too.
-    // ... simplest approach for now would be to make win condition
-    // ... to have all disks, big-to-small from the bottom up, on the rhs pile
-    // ... fancier would be to allow same pile condition but either middle or rhs
+    // display towers now
+    print_disk_scene();
 
-    // valid characters : 'l', 'm', or 'r'
-    // we don't validate move here... we do that in assess()
+    // display player instructions and get player input
+    print_player_prompt();
+    print_spacer();
+    echo(); // helpful for the player to see the command they're typing
     command[0] = getch(); // "from" pile
     command[1] = getch(); // "to" pile
+    noecho();
 
-    game_state = assessing;
+    game_state = assessing; // valid characters : 'l', 'm', or 'r', but we need to check more than just that
 }
 
 void assess()
@@ -206,8 +217,8 @@ void assess()
     char pile_label_from = *from_pile;
     char pile_label_to = *to_pile;
 
-    int lowest_empty_slot_from = lowest_empty_slot(get_pile_by_label(pile_label_from, 0 /* means current, not candidate*/));
-    int lowest_empty_slot_to = lowest_empty_slot(get_pile_by_label(pile_label_to, 0 /* means current, not candidate*/));
+    int lowest_empty_slot_from = lowest_empty_slot(get_pile_by_label(pile_label_from));
+    int lowest_empty_slot_to = lowest_empty_slot(get_pile_by_label(pile_label_to));
     if (lowest_empty_slot_to == -1 || lowest_empty_slot_from == 2) // then player is trying to add to a full pile, or move a null_disk, probably both
     {
         // just act like we didn't hear the command
@@ -217,15 +228,15 @@ void assess()
 
     // then we check (3)
     
-    enum disk* source_pile = get_pile_by_label(pile_label_from, 0 /* means current, not candidate*/);
-    enum disk* destination_pile = get_pile_by_label(pile_label_to, 0 /* means current, not candidate*/);
+    enum disk* source_pile = get_pile_by_label(pile_label_from);
+    enum disk* destination_pile = get_pile_by_label(pile_label_to);
     
     // "top" and "bottom" here refer to the disks that would be respectively on top/bottom if player's command is executed
 
     // already handled case where source pile is empty earlier
     int top_disk_position = lowest_empty_slot(source_pile) + 1;
-    enum disk top_disk = source_pile[top_disk_position];
     int bottom_disk_position = lowest_empty_slot(destination_pile) + 1;
+    enum disk top_disk = source_pile[top_disk_position];
     enum disk bottom_disk = destination_pile[bottom_disk_position];
 
     if (top_disk < bottom_disk || bottom_disk == null_disk) // criterion for legality of the move
@@ -248,20 +259,134 @@ void process()
     // this function assumes if it's being called, that
     // it is only ever being asked to make a valid transition
 
-    // TODO
+    // apply move to game data, don't need a buffer
+    enum disk* source_pile = get_pile_by_label(command[0]);
+    enum disk* destination_pile = get_pile_by_label(command[1]);
+    int top_disk_position = lowest_empty_slot(source_pile) + 1;
+    destination_pile[lowest_empty_slot(destination_pile)] = source_pile[top_disk_position];
+    source_pile[top_disk_position] = null_disk;
+    
+    if (destination_pile[0] == red) // win condition
+    {
+        game_state = won;
+    }
+    else
+    {
+        game_state = waiting;
+    }
+}
+
+void print_spacer()
+{
+    attron(COLOR_PAIR(6)); printw("xxxx"); attron(COLOR_PAIR(1));
+}
+
+void print_null_disk()
+{
+    attron(COLOR_PAIR(6)); printw("xxxxxxxxxxxxxxx"); attron(COLOR_PAIR(1));
+}
+
+void print_red_disk()
+{
+    attron(COLOR_PAIR(6)); printw("xxxx"); attron(COLOR_PAIR(2)); printw("xxxxxxx"); attron(COLOR_PAIR(6)); printw("xxxx"); attron(COLOR_PAIR(1));
+}
+
+void print_orange_disk()
+{
+    attron(COLOR_PAIR(6)); printw("xx"); attron(COLOR_PAIR(4)); printw("xxxxxxxxxxx"); attron(COLOR_PAIR(6)); printw("xx"); attron(COLOR_PAIR(1));
+}
+
+void print_yellow_disk()
+{
+    attron(COLOR_PAIR(3)); printw("xxxxxxxxxxxxxxx"); attron(COLOR_PAIR(1));
+}
+
+void print_base_plate_disk()
+{
+    attron(COLOR_PAIR(5)); printw("xxxxxxxxxxxxxxx"); attron(COLOR_PAIR(1));
+}
+
+void print_disk_scene()
+{
+    // get top row of left, middle and right piles
+    enum disk top_row[3] = {game_data_current.lhs[0], game_data_current.middle[0], game_data_current.rhs[0]};
+
+    // get middle row of left, middle and right piles
+    enum disk middle_row[3] = {game_data_current.lhs[1], game_data_current.middle[1], game_data_current.rhs[1]};
+
+    // get bottom row of left, middle and right piles
+    enum disk bottom_row[3] = {game_data_current.lhs[2], game_data_current.middle[2], game_data_current.rhs[2]};
+
+    void (*disk_printers[4])() = {print_null_disk, print_red_disk, print_orange_disk, print_yellow_disk};
+    
+    // print top row
+    printw("\n");
+    printw("\n");
+    print_spacer(); print_spacer();
+    for (int i = 0; i < 3; i++)
+    {
+        int disk_printer_index = top_row[i];
+        (*disk_printers[disk_printer_index])();
+        print_spacer();
+    }
+    printw("\n");
+
+    // print middle row    
+    print_spacer(); print_spacer();
+    for (int i = 0; i < 3; i++)
+    {
+        int disk_printer_index = middle_row[i];
+        (*disk_printers[disk_printer_index])();
+        print_spacer();
+    }
+    printw("\n");
+    
+    // print bottom row    
+    print_spacer(); print_spacer();
+    for (int i = 0; i < 3; i++)
+    {
+        int disk_printer_index = bottom_row[i];
+        (*disk_printers[disk_printer_index])();
+        print_spacer();
+    }
+    printw("\n");
+    
+    // print base plate row    
+    print_spacer(); print_spacer(); print_base_plate_disk(); print_spacer(); print_base_plate_disk(); print_spacer(); print_base_plate_disk();
+    printw("\n");
+}
+
+void print_player_prompt()
+{
+    printw("\n\n");
+    print_spacer();
+    printw("To win: stack the disks up from biggest to smallest on a new pile.\n\n");
+    print_spacer();
+    printw("Rules:\n");
+    print_spacer();
+    printw("(1) move only one disk at a time\n");
+    print_spacer();
+    printw("(2) a bigger disk cannot go on top of a smaller disk\n\n");
+    print_spacer();
+    printw("To move:\n");
+    print_spacer();
+    printw("\"rm\" moves top disk from right pile to middle pile,\n");
+    print_spacer();
+    printw("\"ml\" moves from middle to left, etc.\n\n");
+    print_spacer();
+    printw("Enter your move: ");
 }
 
 void congratulate()
 {
-    // get rid of anything currently being displayed
     clear();
 
+    attron(COLOR_PAIR(1));
     printw(".==========================.\n");
     printw("|   THE TOWERS OF HANOI    |\n");
     printw(".==========================.\n");
     printw("\nCongratulations!\nYou have won the game.\nPress any key to exit.");
     
-    // essentially a buffer swap
     refresh();
 
     // wait until a user has pressed a key to exit
@@ -275,36 +400,30 @@ int lowest_empty_slot(pile p)
     {
         return -1;
     }
-    else if (p[0] == null_disk)
+    else if (p[2] == null_disk)
     {
-        return 0;
+        return 2;
     }
     else if (p[1] == null_disk)
     {
         return 1;
     }
-    return 2; // means pile is completely empty
+    return 0;
 }
 
-bool is_stacked_properly(pile p)
-{
-    enum disk top = p[0], middle = p[1], bottom = p[2];
-    return bottom > middle && middle > top;
-}
-
-enum disk* get_pile_by_label(char c, bool should_return_candidate_pile)
+enum disk* get_pile_by_label(char c)
 {
     if (c == 'l')
     {
-        return should_return_candidate_pile ? game_data_candidate_new.lhs : game_data_current.lhs;
+        return game_data_current.lhs;
     }
     else if (c == 'm')
     {
-        return should_return_candidate_pile ? game_data_candidate_new.middle : game_data_current.middle;
+        return game_data_current.middle;
     }
     else if (c == 'r')
     {
-        return should_return_candidate_pile ? game_data_candidate_new.rhs : game_data_current.rhs;
+        return game_data_current.rhs;
     }
     return NULL;
 }
